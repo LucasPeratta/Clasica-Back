397259d8362c5b3fb5334a1df479fb4957f8116d
Lucas Peratta <lucasperatta@gmail.com>
2025-12-23 01:17:44 -0300

    \Fix multer types with as any cast"

--- src/routes/pax/index.ts
+++ src/routes/pax/index.ts
@@ -14,14 +14,14 @@ import {
 import { upload } from "../../middlewares/upload";
 
 router.get("/", getAllPax);
-router.post("/create", upload.any(), addPax);
+router.post("/create", upload.any() as any, addPax);
 router.get("/:id", getPaxById);
-router.put("/update/:id", upload.any(), updatePax);
+router.put("/update/:id", upload.any() as any, updatePax);
 router.delete("/:id", deletePax);
 
 // Photo management routes
 router.get("/:id/photos", getPaxPhotos);
-router.post("/:id/photos", upload.any(), addPhotoToPax);
+router.post("/:id/photos", upload.any() as any, addPhotoToPax);
 router.delete("/:paxId/photos/:photoId", deletePhotoFromPax);
 
 export default router;
